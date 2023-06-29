760f576b3928e54ca8d942f1e596f72cf88b5c80
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-06-29 20:31:38 +0200

    Building navigations

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import RouterLink from "vue-router"
+
+
+</script>
+
+
+<template>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <img src="../assets/Vue_Logo_Black.png" alt="">
+        <h1>Vue Todos</h1>
+      </div>
+      <ul class="nav-routes">
+        <RouterLink to=""></RouterLink>
+      </ul>
+
+    </nav>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
