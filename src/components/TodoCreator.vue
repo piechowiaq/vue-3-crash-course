2d5a0ff577adc4c8c3d74f76af4649abddc7c057
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-07-07 21:02:21 +0200

    Styling error messages

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -25,11 +25,11 @@ const createTodo = () => {
 </script>
 
 <template>
-  <div class="input-wrap">
+  <div class="input-wrap" :class="{'input-err' : todoState.invalid}">
     <input type="text" v-model="todoState.todo" />
     <button @click="createTodo">Create</button>
   </div>
-  <p class="err-msg" v-if="todoState.invalid">{{todoState.errMsg}}</p>
+  <p class="err-msg" v-show="todoState.invalid">{{todoState.errMsg}}</p>
 </template>
 
 <style lang="scss" scoped>
@@ -38,7 +38,11 @@ const createTodo = () => {
   transition: 250ms ease;
   border: 2px solid #41b080;
 
-&:focus-within {
+  &.input-err {
+    border-color: red;
+  }
+
+  &:focus-within {
    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
    0 -2px 4px -2px rgb(0 0 0 / 0.1);
  }
@@ -57,5 +61,12 @@ button {
   padding: 8px 16px;
   border: none;
 }
+}
+
+.err-msg {
+  margin-top: 6px;
+  font-size: 12px;
+  text-align: center;
+  color: red;
 }
 </style>
