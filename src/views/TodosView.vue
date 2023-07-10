f753377f319009819fcc0111ce89123038aa432e
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-07-10 20:11:39 +0200

    Saving todolist to local storage

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -8,30 +8,49 @@ import TodoItem from "../components/TodoItem.vue";
 
 const todoList = ref([])
 
+const fetchTodoList = () => {
+  const savedTodoList = JSON.parse(localStorage.getItem("todoList"))
+
+  if(savedTodoList) {
+    todoList.value = savedTodoList;
+  }
+}
+
+fetchTodoList();
+
+const setTodoListLocalStorage = () => {
+  localStorage.setItem("todoList", JSON.stringify(todoList.value))
+}
+
 const createTodo = (todo) => {
   todoList.value.push({
     id: uid(),
     todo,
     isCompleted : null,
     isEditing:  null
-  })
+  });
+  setTodoListLocalStorage()
 
 }
 
 const toggleTodoComplete = (todoPos) => {
-  todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted
+  todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+  setTodoListLocalStorage()
 }
 
 const toggleEditTodo = (todoPos) => {
-  todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing
+  todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing;
+  setTodoListLocalStorage()
  }
 
 const updateTodo = (todoVal, todoPos ) => {
-  todoList.value[todoPos].todo = todoVal
+  todoList.value[todoPos].todo = todoVal;
+  setTodoListLocalStorage()
 }
 
 const deleteTodo = (todoId) => {
   todoList.value = todoList.value.filter((todo) => todo.id !== todoId)
+  setTodoListLocalStorage()
 }
 
 </script>
